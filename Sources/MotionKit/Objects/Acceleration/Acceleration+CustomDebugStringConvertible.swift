f3cac0694a759d398a95b50316f2cf9288cc16d7
Alexandre <alexandresaad@icloud.com>
2021-12-06 19:38:15 +0100

    Commit

---
+++ Sources/MotionKit/Objects/Acceleration/Acceleration+CustomDebugStringConvertible.swift
@@ -0,0 +1,12 @@
+// Acceleration+CustomDebugStringConvertible.swift
+// MotionKit
+//
+// Copyright © 2021 A. H. de Quatre Ltd.
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+
+extension Acceleration: CustomDebugStringConvertible {
+	public var description: String {
+		return "x: \(self.x), y: \(self.y), z: \(self.z)"
+	}
+}
