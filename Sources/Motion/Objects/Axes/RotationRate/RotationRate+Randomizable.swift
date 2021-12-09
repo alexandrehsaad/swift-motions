ab72c5164617e72cd47eabc736742fe7823320d3
Alexandre H. Saad <alexandresaad@icloud.com>
2021-12-09 16:53:58 +0100

    Commit

--- Sources/Motion/Objects/Axes/RotationRate/RotationRate+Randomizable.swift
+++ Sources/Motion/Objects/Axes/RotationRate/RotationRate+Randomizable.swift
@@ -9,7 +9,7 @@ extension RotationRate {
 	///
 	/// - Parameter range: The range in which to create a random value.
 	/// - Returns: A random instance within the bounds of the range.
-	static func random(in range: ClosedRange<Double> = -1...1) -> Self {
+	public static func random(in range: ClosedRange<Double> = -1...1) -> Self {
 		return self.init(
 			x: .random(in: range),
 			y: .random(in: range),
