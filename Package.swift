93839a4509d749550a0edc8427f3f4f554300bc9
Alexandre <alexandresaad@icloud.com>
2022-03-29 09:56:21 +0200

    Commit

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -8,10 +8,10 @@ let package = Package(
     products: [
         .library(name: "Motion", targets: ["Motion"])
     ], dependencies: [
-		.package(name: "swift-extensions", url: "https://github.com/alexandrehsaad/swift-extensions", branch: "main")
+		.package(url: "https://github.com/alexandrehsaad/swift-extensions", branch: "main")
 	], targets: [
 		.target(name: "Motion", dependencies: [
 			.product(name: "Extensions", package: "swift-extensions"),
 		]),
-    ], swiftLanguageVersions: [.version("5.5")]
+	], swiftLanguageVersions: [.v5]
 )
