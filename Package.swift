c326a41e61a34b689c4bcbcf1e1fb90d8045e153
Alexandre <alexandresaad@icloud.com>
2022-03-31 10:51:31 +0200

    Commit

--- Package.swift
+++ Package.swift
@@ -5,13 +5,22 @@ import PackageDescription
 let package = Package(
     name: "swift-motions",
     defaultLocalization: "en",
+	platforms: [
+		.iOS(.v9),
+		.macCatalyst(.v13),
+		.macOS(.v10_15),
+		.watchOS(.v2)
+	],
     products: [
         .library(name: "Motions", targets: ["Motions"])
-    ], dependencies: [
+    ],
+	dependencies: [
 		.package(url: "https://github.com/alexandrehsaad/swift-extensions", branch: "main")
-	], targets: [
+	],
+	targets: [
 		.target(name: "Motions", dependencies: [
 			.product(name: "Extensions", package: "swift-extensions"),
 		]),
-	], swiftLanguageVersions: [.v5]
+	],
+	swiftLanguageVersions: [.v5]
 )
--- Sources/Motions/MotionManager/AuthorizationStatus/AuthorizationStatus.swift
+++ Sources/Motions/MotionManager/AuthorizationStatus/AuthorizationStatus.swift
@@ -4,26 +4,47 @@
 // Copyright © 2022 Alexandre H. Saad
 //
 
+#if !os(macOS)
+
 /// A representation of a motion manager authorization status.
 public enum AuthorizationStatus {
+	/// The status is not known.
+	@available(iOS, obsoleted: 11)
+	@available(macCatalyst, obsoleted: 13.1)
+	@available(watchOS, obsoleted: 4)
+	case unknown
+	
 	/// The status has not yet been determined.
+	@available(iOS 11, macCatalyst 13.1, watchOS 4, *)
 	case undetermined
 	
 	/// Access is denied and restricted by the device.
+	@available(iOS 11, macCatalyst 13.1, watchOS 4, *)
 	case restricted
 	
 	/// Access is denied by the user.
+	@available(iOS 11, macCatalyst 13.1, watchOS 4, *)
 	case denied
 	
 	/// Access is authorized by the user.
 	case authorized
+	
+	/// A boolean value indicating whether access is authorized by the user.
+	public var isAuthorized: Bool {
+		switch self {
+		case .authorized:
+			return true
+		default:
+			return false
+		}
+	}
 }
 
 #if canImport(CoreMotion)
 
 import CoreLocation
 
-@available(iOS 11.0, macOS 10.5, watchOS 4, *)
+@available(iOS 11, macCatalyst 13.1, watchOS 4, *)
 extension CMAuthorizationStatus {
 	///
 	internal var clone: AuthorizationStatus {
@@ -41,5 +62,7 @@ extension CMAuthorizationStatus {
 		}
 	}
 }
+
+#endif
 
 #endif
